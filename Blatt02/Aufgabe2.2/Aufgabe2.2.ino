1818a0e08a2f7f5b37f094340203c49831f51caa
Frederik Wille <frederik@wille.hamburg>
2015-04-29 17:42:05 +0000

    2.2 done

--- Blatt02/Aufgabe2.2/Aufgabe2.2.ino
+++ Blatt02/Aufgabe2.2/Aufgabe2.2.ino
@@ -1,16 +1,17 @@
-int pin_Standby = 13;
-int pin_Motor_In2 = 12;
-int pin_Motor_In1 = 11;
-int pin_Motor_PWM = 10;
-int PWM_Value = 0;
-int mode = 0; // 0 = steigend CW, 1 = fallend CW, 2/3 = CCW
+int pin_Standby = 5;
+int pin_Motor_In2 = 4;
+int pin_Motor_In1 = 3;
+int pin_Motor_PWM = 2;
+volatile int PWM_Value = 0;
+volatile int mode = 0; // 0 = steigend CW, 1 = fallend CW, 2/3 = CCW
 
 void setup() {
   pmc_set_writeprotect(false);
   pmc_enable_periph_clk(ID_TC7);
   
+  
+  TC_Configure(TC2, 1, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK4);
   TC2->TC_CHANNEL[1].TC_RC = 12868;
-  TC_Configure(TC2, 0, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK4);
   TC2->TC_CHANNEL[1].TC_IER = TC_IER_CPCS;
   TC2->TC_CHANNEL[1].TC_IDR = ~TC_IER_CPCS;
 
@@ -28,60 +29,66 @@ void setup() {
   digitalWrite(pin_Standby, HIGH);
 
   Serial.begin(9600);
+  analogWrite(pin_Motor_PWM, PWM_Value);
 }
 
 void loop() {
   Serial.println(mode);
   Serial.println(PWM_Value);
   delay(100);
+  analogWrite(pin_Motor_PWM, PWM_Value);
 }
 
-void TC7_Handler() {
-  uint32_t status;
-  status = TC2->TC_CHANNEL[1].TC_SR;
-  Serial.println("handler");
+void TC7_Handler(void) {
+  //uint32_t status = TC2->TC_CHANNEL[1].TC_SR;
+  TC_GetStatus(TC2, 1);
+  //Serial.println("handler");
+
+  //analogWrite(pin_Motor_PWM, PWM_Value);
+  
   if (mode == 0)
   {
+    //Serial.println("Mode1");
     PWM_Value += 1;
-    if (PWM_Value > 255) {
+    if (PWM_Value >= 255) {
       PWM_Value = 255;
       mode = 1;
       digitalWrite(pin_Motor_In1, HIGH);
       digitalWrite(pin_Motor_In2, LOW);
     }
-    analogWrite(pin_Motor_PWM, PWM_Value);
   }
-  else if (mode == 1)
+  if (mode == 1)
   {
-    PWM_Value -= 1;
-    if (PWM_Value < 0) {
+    //Serial.println("Mode2");
+    --PWM_Value;
+    if (PWM_Value <= 0) {
       PWM_Value = 0;
       mode = 2;
       digitalWrite(pin_Motor_In1, LOW);
       digitalWrite(pin_Motor_In2, HIGH);
     }
-    analogWrite(pin_Motor_PWM, PWM_Value);
-  }
-  else if (mode == 2)
+  } else if (mode == 2)
   {
+    //Serial.println("Mode3");
     PWM_Value += 1;
-    if (PWM_Value < 255) {
+    if (PWM_Value <= 255) {
       PWM_Value = 255;
       mode = 3;
       digitalWrite(pin_Motor_In1, LOW);
       digitalWrite(pin_Motor_In2, HIGH);
     }
-    analogWrite(pin_Motor_PWM, PWM_Value);
-  }
-  else if (mode == 3)
+  }else if (mode == 3)
   {
     PWM_Value -= 1;
-    if (PWM_Value < 0) {
+    if (PWM_Value <= 0) {
       PWM_Value = 0;
       mode = 0;
       digitalWrite(pin_Motor_In1, HIGH);
       digitalWrite(pin_Motor_In2, LOW);
     }
-    analogWrite(pin_Motor_PWM, PWM_Value);
-  }
+  } 
+  //Serial.println("handler2");
+  //status = TC2->TC_CHANNEL[1].TC_SR;
+  //TC_GetStatus(TC2, 1);
+  //status = Serial.println(status);
 }
