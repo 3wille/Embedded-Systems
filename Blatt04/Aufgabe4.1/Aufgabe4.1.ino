b427b1fae3f6841b3420deadd91ccffd74370de4
Frederik Wille <frederik@wille.hamburg>
2015-06-02 14:46:18 +0200

    Aufgabe4.1

--- Blatt04/Aufgabe4.1/Aufgabe4.1.ino
+++ Blatt04/Aufgabe4.1/Aufgabe4.1.ino
@@ -1,10 +1,13 @@
-int master = 1;
-int pin_led = 13;
-int pin_button = 12;
+int master = 0; // master=1, slave=0
+int pin_led = 12;
+int pin_button = 11;
+int pin_intern_led = 13;
 
 int button_count = 0;
 int brightness = 0;
 int rising = 1;
+
+char str[3];
 
 
 void setup(){
@@ -26,36 +29,68 @@ void setup(){
 		TC_Start(TC2, 0);
 
 		pinMode(pin_button, INPUT);
+		pinMode(pin_intern_led, OUTPUT);
+		digitalWrite(pin_intern_led, 0);
+
 	}else{
-			pinMode(pin_led, OUTPUT);
+		pinMode(pin_led, OUTPUT);
+		analogWrite(pin_led, 0);
 	}
+
+
+
 	Serial.println("Setup complete");
 }
-
+/*
 void loop(){
 	if(master==1){
-	    Serial3.print(brightness);
+		Serial3.write(brightness);
 	}else{
-		analogWrite(pin_led, Serial3.read());
+    	if(Serial3.available()>0){
+    		
+		}
+		//Serial.println(str);
+		analogWrite(pin_led, brightness);
 	}
+	Serial.println(brightness);
 	delay(100);
+}*/
+void loop(){
+	if(Serial3.available()>0){
+	    if(master==1){
+	        int recv = Serial3.read();
+	        if(recv == 1){
+	            master_led_foo();
+	        }
+	    }else if(master==0){
+	    	int recv = Serial3.read();
+	    	if(recv == 1){
+	    		slave_led_foo();
+	    	}
+	    }
+	}
 }
 
 void TC6_Handler()
 {
+	//Serial.println("Handler");
 	uint32_t status;
 	status = TC2->TC_CHANNEL[0].TC_SR;
 
-  	if (digitalRead(pin_button) == LOW)
-  	{
-  		++button_count;
-  		if (button_count >= 30) {
-  			pressed();
-  			button_count = 0;
-  		}
-  	}
+	if (digitalRead(pin_button) == LOW)
+	{
+		//Serial.println("LOW");
+		++button_count;
+		if (button_count >= 30) {
+			sendOne();
+			button_count = 0;
+		}
+	}else if(digitalRead(pin_button) == HIGH){
+		//Serial.println("HIGH");
+		button_count = 0;
+	}
 }
-
+/*
 void pressed(){
 	if(rising == 1){
 		brightness += 10;
@@ -69,5 +104,36 @@ void pressed(){
 	if(brightness<=0){
 		brightness = 0;
 		rising = 1;
+	}
+} */
+
+
+void sendOne(){
+	Serial3.write(1);
+}
+
+void slave_led_foo(){
+	//int i = 0;
+	for(int i=0;i<256;i++){
+		analogWrite(pin_led, i);
+		delay(50);
+		Serial.println(i);
+	}
+	sendOne();
+	for(int i=255;i>0;i--){
+	    analogWrite(pin_led, i);
+	    delay(50);
+	    Serial.println(i);
+	}
+	sendOne();
+}
+
+void master_led_foo(){
+	int i;
+	for(i=0; i<3; i++){
+	    digitalWrite(pin_intern_led, HIGH);
+	    delay(50);
+	    digitalWrite(pin_intern_led, LOW);
+	    delay(50);
 	}
 }
